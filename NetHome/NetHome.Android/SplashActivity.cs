7e9a4ab9d479b23f11ab341bc17b2846a75db192
klebber <bstojev199@gmail.com>
2021-10-18 00:46:07 +0200

    Implement splash screen for Android

---
+++ NetHome/NetHome.Android/SplashActivity.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetHome.Droid
+{
+    [Activity(Theme = "@style/SplashTheme", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { Startup(); });
+            startupWork.Start();
+        }
+
+        public override void OnBackPressed() { }
+
+        private async void Startup()
+        {
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
